9957bb2246258cdaa4a59a00f01ec0cf72fbb1ed
kinachan <army1102@gmail.com>
2022-04-25 16:55:04 +0900

    columnInfo追加

--- index.js
+++ index.js
@@ -2,12 +2,40 @@
 
 const file = document.getElementById('file-upload');
 const tableNameElement = document.getElementById('table-name');
+const columnInfoElem = document.getElementById('column-info');
+let columnInfo = null;
 
 function str2Array(str) {
   var array = [],i,il=str.length;
   for(i=0;i<il;i++) array.push(str.charCodeAt(i));
   return array;
 }
+
+const toCsvText = (result) => {
+  const sjisArray = str2Array(result);
+  const uniArray = Encoding.convert(sjisArray, 'UNICODE', 'SJIS');
+  const text = Encoding.codeToString(uniArray);
+
+  return text;
+}
+
+
+columnInfoElem.addEventListener('change', (ev) => {
+  const read = new FileReader();
+  reader.readAsBinaryString(ev.target.files[0]);
+
+  reader.onload = (event) => {
+    const text = toCsvText(event.target.result);
+    const rows = text.split(/\n/);
+    const detail = rows.splice(1);
+
+    const results = detail.map(d => ({
+      name: d[0],
+      type: d[1],
+    }));
+    columnInfo = results;
+  };
+})
 
 
 file.addEventListener('change', (ev) => {
@@ -17,14 +45,11 @@ file.addEventListener('change', (ev) => {
   reader.readAsBinaryString(ev.target.files[0]);
 
   reader.onload = (event) => {
-    const sjisArray = str2Array(event.target.result);
-    const uniArray = Encoding.convert(sjisArray, 'UNICODE', 'SJIS');
-    const text = Encoding.codeToString(uniArray);
+    const text = toCsvText(event.target.result);
     
     const rows = text.split(/\n/);
     const headerArray = rows[0].split(',');
     const header = headerArray.map(x => x.replace(/"/g, ''));
-    console.log(`[log] header.length is ${header.length} `);
     const body = rows.splice(1);
 
     const sqlSentence = `INSERT INTO ${tableName} (${header.join(',')})`;
